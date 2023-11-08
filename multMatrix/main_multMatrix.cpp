423559b2408f61fdafd5fa7fc4b9341cfa569564
pauss12 <paulamendezsv@gmail.com>
2023-11-08 13:44:42 +0100

    añadido el main que viene al principio

--- multMatrix/main_multMatrix.cpp
+++ multMatrix/main_multMatrix.cpp
@@ -17,31 +17,17 @@ int main()
 
     //lo de debajo lo he añadido yo para probar
     mmatrix->escribirMatriz(m1, "pruebaCrearRandom.txt");
-    /*
+
     matrix_t* m2= mmatrix->crearIdentidad(5,5);
     matrix_t* mres=mmatrix->multiplicarMatrices(m1,m2);
-    */
-    matrix_t *mres = new matrix_t();
-
-    //Inventarme valores para mres
-    mres->rows = 5;
-    mres->cols = 5;
-    mres->data = new int[10];
-    for(int i=0;i<10;i++)
-    {
-        mres->data[i] = i;
-    }
-
     mmatrix->escribirMatriz(mres,"HOLA.txt");
     matrix_t* m3=mmatrix->leerMatriz("resultado.txt");
-    /*
-        matrix_t* mres2=mmatrix->multiplicarMatrices(m1,m3);
-        mmatrix->escribirMatriz(mres2,"resultado2.txt");
+    matrix_t* mres2=mmatrix->multiplicarMatrices(m1,m3);
+    mmatrix->escribirMatriz(mres2,"resultado2.txt");
 
-        freeMatrix(m1);
+    freeMatrix(m1);
 
-        freeMatrix(m2);
-        */
+    freeMatrix(m2);
     freeMatrix(mres);
     
     freeMatrix(m3);
@@ -49,3 +35,26 @@ int main()
 
     delete mmatrix;
 }
+
+//MAIN QUE NOS DAN AL PRINCIPIO, EL QUE DESCOMENTARE DESPUES DE LAS PRUEBAS
+/*
+int main()
+{
+    multMatrix *mmatrix = new multMatrix();
+
+    matrix_t *m1 = mmatrix->crearRandom(5, 5);
+    matrix_t *m2 = mmatrix->crearIdentidad(5, 5);
+    matrix_t *mres = mmatrix->multiplicarMatrices(m1, m2);
+    mmatrix->escribirMatriz(mres, "resultado.txt");
+    matrix_t *m3 = mmatrix->leerMatriz("resultado.txt");
+    matrix_t *mres2 = mmatrix->multiplicarMatrices(m1, m3);
+    mmatrix->escribirMatriz(mres2, "resultado2.txt");
+
+    freeMatrix(m1);
+    freeMatrix(m2);
+    freeMatrix(mres);
+    freeMatrix(m3);
+    freeMatrix(mres2);
+    delete mmatrix;
+}
+*/
