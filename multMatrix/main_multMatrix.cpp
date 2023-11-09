efd8da016a785349679aaeaee5433cbdb55dc1e2
pauss12 <paulamendezsv@gmail.com>
2023-11-10 00:25:21 +0100

    Main limpio de multMatrix

--- multMatrix/main_multMatrix.cpp
+++ multMatrix/main_multMatrix.cpp
@@ -8,35 +8,6 @@ void freeMatrix(matrix_t* m){
     delete[] m;
 
 }
-/*
-int main()
-{
-    multMatrix_stub *mmatrix=new multMatrix_stub();
-
-    matrix_t* m1= mmatrix->crearRandom(5,5);
-
-    //lo de debajo lo he añadido yo para probar
-    mmatrix->escribirMatriz(m1, "pruebaCrearRandom.txt");
-
-    matrix_t* m2= mmatrix->crearIdentidad(5,5);
-    matrix_t* mres=mmatrix->multiplicarMatrices(m1,m2);
-    mmatrix->escribirMatriz(mres,"HOLA.txt");
-    matrix_t* m3=mmatrix->leerMatriz("resultado.txt");
-    matrix_t* mres2=mmatrix->multiplicarMatrices(m1,m3);
-    mmatrix->escribirMatriz(mres2,"resultado2.txt");
-
-    freeMatrix(m1);
-
-    freeMatrix(m2);
-    freeMatrix(mres);
-
-    freeMatrix(m3);
-    freeMatrix(mres2);
-
-    delete mmatrix;
-}
-*/
-// MAIN QUE NOS DAN AL PRINCIPIO, EL QUE DESCOMENTARE DESPUES DE LAS PRUEBAS
 
 int main()
 {
